00a65754f6aba809c8e93ed1980bd0a294986f7f
Kramzin <kramzin.ru@gmail.com>
2021-07-15 01:51:10 +0300

    Imolement Header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdl2ZPX6Yup--A4R5-x2Q_yzErQy674oo",
+    authDomain: "fir-fd3ba.firebaseapp.com",
+    projectId: "fir-fd3ba",
+    storageBucket: "fir-fd3ba.appspot.com",
+    messagingSenderId: "884939073620",
+    appId: "1:884939073620:web:6c0a775c52c77b4c91e5c1"
+  };
