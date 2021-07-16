575eb943a87dd603da6452fc5c884e03a7b040b4
Kramzin <kramzin.ru@gmail.com>
2021-07-16 19:15:39 +0300

    Implement checkout

--- src/Home.js
+++ src/Home.js
@@ -13,18 +13,48 @@ function Home() {
         />
 
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            title="The lean startup"
+            price={29.99}
+            image="https://www.investopedia.com/thmb/eRKJOys7Ff9GRkgg3kW2uZXbNDo=/900x600/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-527398042-0aa90c3c871d4b98883700135c9013de.jpg"
+            rating={5}
+          />
+          <Product
+            title="Lenovo Chromebook S330 Laptop, 14-Inch FHD (1920 x 1080) Display, MediaTek MT8173C Processor, 4GB LPDDR3, 64GB eMMC, Chrome OS, 81JW0000US, Business Black"
+            price={237.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/61ViOMIUhmL._AC_SL1500_.jpg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            title="Fact check"
+            price={229.99}
+            image="https://miro.medium.com/max/1400/1*Oo1TOrs6RA1gk61cba_9ng.jpeg"
+            rating={5}
+          />{" "}
+          <Product
+            title="Fact check"
+            price={229.99}
+            image="https://miro.medium.com/max/1400/1*Oo1TOrs6RA1gk61cba_9ng.jpeg"
+            rating={5}
+          />{" "}
+          <Product
+            title="Fact check"
+            price={229.99}
+            image="https://miro.medium.com/max/1400/1*Oo1TOrs6RA1gk61cba_9ng.jpeg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-          <Product />
+          <Product
+            title="XFX Radeon RX 580 GTS XXX Edition 1386MHz OC+, 8GB GDDR5, VR Ready, Dual BIOS, 3xDP HDMI DVI, AMD Graphics Card (RX-580P8DFD6)"
+            price={774.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81D46VNLb9L._AC_SL1500_.jpg"
+            rating={4}
+          />
         </div>
       </div>
     </div>
---
+++ src/Subtotal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              <h2>
+                <strong className="subtotal__sub">Subtotal (0 items):</strong>
+              </h2>{" "}
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button> Proceed to Checkout </button>
+    </div>
+  );
+}
+
+export default Subtotal;
