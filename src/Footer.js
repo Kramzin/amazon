9aa64fd8736f5de005131ca543e96aaff59203f1
Kramzin <kramzin.ru@gmail.com>
2021-10-01 20:18:32 +0300

    Implement payment

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,9 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => (
   <div className="footer">
-    <p>Amazon Music Stream millions of songs</p>
-    <p> Alexa Actionable Analytics for the Web</p>
-    <p>Audible Listen to Books & Original Audio Performances</p>
-    <p>Sell on Amazon Start a Selling Account</p>
+    Made by <a href="https://github.com/Kramzin/">Kramzin</a>
   </div>
 );
 
